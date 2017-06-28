9221bade7a97405307ea112b4a1ca730dc9e38a3
Dayves Oliveira <dayvesoliveira@gmail.com>
2017-06-28 14:04:05 -0300

    updated

--- src/app/dribbble-app/services/dribbble.service.ts
+++ src/app/dribbble-app/services/dribbble.service.ts
@@ -13,9 +13,7 @@ export class DribbbleService {
 
     configServer = {
         url:    'https://api.dribbble.com/v1/shots',
-        TOKEN:  'Bearer fffb255434cf4ffd8d4b036614f864c18bd07a2d34aadc8541e6af3c6919292f'
-        //fffb255434cf4ffd8d4b036614f864c18bd07a2d34aadc8541e6af3c6919292f
-        //e0631e6461043841566e0c44768a31436421b5b11c82046847e01259f67f7ec4
+        TOKEN:  'Bearer XXXXXXX'
     };
 
     constructor (http: Http) {
